e7836656e02241e45fb58374fc3cf2e5d9510a17
인준 <cij041109@gmail.com>
2026-07-31 22:28:47 +0900

    feat: Spring Boot 백엔드 프로젝트 초기 설정 (#10)

---
+++ BE/src/test/java/com/onboardos/onboarding/OnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onboardos.onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
